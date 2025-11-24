766011b21afa6316bd88fb63627fdb93efc0d382
IsmaCoder024 <ismanmandai024@gmail.com>
2025-11-24 16:49:25 +0300

    Ready

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -5,26 +5,32 @@ import { Menu, X } from "lucide-react"; // icons
 import { NavLink } from "react-router-dom";
 
 function Navigation({ links }) {
-
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      
       <button
         className="menu-toggle"
         onClick={() => setOpen(!open)}
         aria-label="Toggle Menu"
       >
-        {open ? <X size={24} /> : <><Menu size={24} /> Menu</>}
+        {open ? (
+          <X/>
+        ) : (
+          <>
+            <Menu/> Menu
+          </>
+        )}
       </button>
-
-      <div className={`top-items ${open ? "open" : ""}`}>
-        {links.map((link, i) => (
-          <a key={i}>
-            <NavLink to={link.path}>{link.label}</NavLink>
-          </a>
-        ))}
+      
+      <div className="top-board"> 
+        <div className={`top-items ${open ? "open" : ""}`}>
+          {links.map((link, i) => (
+            <a key={i}>
+              <NavLink to={link.path}>{link.label}</NavLink>
+            </a>
+          ))}
+        </div>
       </div>
     </>
   );
